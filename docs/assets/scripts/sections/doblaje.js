9fe3a626e9b09ca4376f209e88e3298c5a640f72
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-10-22 18:41:29 +0200

    Update component URLs to include 'victormoreira'

--- docs/assets/scripts/sections/doblaje.js
+++ docs/assets/scripts/sections/doblaje.js
@@ -1,11 +1,11 @@
 $(function () {
 
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'hero', url: '/assets/sections/doblaje/hero-sub.html' },
-    { id: 'demos', url: '/assets/sections/doblaje/demos-sub.html', callback: initPortfolio  },
-    { id: 'services', url: '/assets/sections/doblaje/services-sub.html' },
+    { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar },
+    { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'hero', url: '/victormoreira/assets/sections/doblaje/hero-sub.html' },
+    { id: 'demos', url: '/victormoreira/assets/sections/doblaje/demos-sub.html', callback: initPortfolio  },
+    { id: 'services', url: '/victormoreira/assets/sections/doblaje/services-sub.html' },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 });
 
