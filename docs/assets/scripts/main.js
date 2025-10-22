3b763e8e353dbcccbf7b9041814b3a64fb374711
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-10-22 18:42:04 +0200

    Update main.js

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -13,11 +13,11 @@
 
 $(function () {
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar, },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'hero', url: '/assets/sections/landing/hero.html' },
-    { id: 'services', url: '/assets/sections/landing/services.html' },
-    { id: 'demos', url: '/assets/sections/landing/demos.html', callback: initDemos },
-    { id: 'contact', url: '/assets/sections/landing/contact.html', callback: initContact },
+    { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar, },
+    { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'hero', url: '/victormoreira/assets/sections/landing/hero.html' },
+    { id: 'services', url: '/victormoreira/assets/sections/landing/services.html' },
+    { id: 'demos', url: '/victormoreira/assets/sections/landing/demos.html', callback: initDemos },
+    { id: 'contact', url: '/victormoreira/assets/sections/landing/contact.html', callback: initContact },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 });
