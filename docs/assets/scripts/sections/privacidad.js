ada9082162bb7d94b5c6c58f5158c49a1e230d59
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-10-22 18:41:49 +0200

    Update privacidad.js

--- docs/assets/scripts/sections/privacidad.js
+++ docs/assets/scripts/sections/privacidad.js
@@ -1,8 +1,8 @@
 $(function () {
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'legal', url: '/assets/sections/aviso-legal/privacidad.html', callback: initLegal },
+    { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar },
+    { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'legal', url: '/victormoreira/assets/sections/aviso-legal/privacidad.html', callback: initLegal },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 
 });
