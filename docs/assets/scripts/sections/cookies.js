ca2359031a4a9ea8df6a0aa01f85637e5fc4f893
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 22:04:14 +0100

    Update cookies.js

--- docs/assets/scripts/sections/cookies.js
+++ docs/assets/scripts/sections/cookies.js
@@ -2,7 +2,7 @@ $(function () {
   [
     { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar },
     { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
-    { id: 'legal', url: '/victormoreira/assets/sections/aviso-legal/cookies.html', callback: initLegal },
+    { id: 'legal', url: '/victormoreira/assets/sections/aviso-legal/cookies.html', callback: initLegal }, 
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 
 });
