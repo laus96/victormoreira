4a1daa0f4c0e20c18a6618a68e6206035ed43307
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 22:30:53 +0100

    Update privacidad.js

--- docs/assets/scripts/sections/privacidad.js
+++ docs/assets/scripts/sections/privacidad.js
@@ -1,19 +1,8 @@
 $(function () {
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'legal', url: '/assets/sections/aviso-legal/privacidad.html', callback: initLegal },
+    { id: 'navbar', url: '/victoremoreira/assets/components/navbar.html', callback: initNavbar },
+    { id: 'footer', url: '/victoremoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'legal', url: '/victoremoreira/assets/sections/aviso-legal/privacidad.html', callback: initLegal },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 
 });
-
-/*
-$(function () {
-  [
-    { id: 'navbar', url: '/victor-doblaje/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/victor-doblaje/components/footer.html', callback: initFooter },
-    { id: 'legal', url: '/victor-doblaje/sections/aviso-legal/privacidad.html', callback: initLegal },
-  ].forEach(c => loadComponent(c.id, c.url, c.callback));
-
-});
-*/
