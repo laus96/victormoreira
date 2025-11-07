03c4cbf46a03aa9b33c442d4977bf5e73f688543
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 22:08:26 +0100

    Update legal.js

--- docs/assets/scripts/sections/legal.js
+++ docs/assets/scripts/sections/legal.js
@@ -3,6 +3,6 @@ $(function () {
     { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar },
     { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
     { id: 'legal', url: '/victormoreira/assets/sections/aviso-legal/aviso.html', callback: initLegal },
-  ].forEach(c => loadComponent(c.id, c.url, c.callback));
+  ].forEach(c => loadComponent(c.id, c.url, c.callback)); 
 
 });
