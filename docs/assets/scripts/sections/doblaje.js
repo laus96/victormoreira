8d40c10ea9082e81b4b96c31e4dc1eb3855ee8cf
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 22:05:35 +0100

    Update doblaje.js

--- docs/assets/scripts/sections/doblaje.js
+++ docs/assets/scripts/sections/doblaje.js
@@ -5,7 +5,7 @@ $(function () {
     { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
     { id: 'hero', url: '/victormoreira/assets/sections/doblaje/hero-sub.html' },
     { id: 'demos', url: '/victormoreira/assets/sections/doblaje/demos-sub.html', callback: initPortfolio  },
-    { id: 'services', url: '/victormoreira/assets/sections/doblaje/services-sub.html' },
+    { id: 'services', url: '/victormoreira/assets/sections/doblaje/services-sub.html' }, 
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 });
 
