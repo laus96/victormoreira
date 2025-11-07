4659ae062d072690ab8acddc6094f00b265a7ba0
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 23:24:47 +0100

    Update main.js

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,23 +1,10 @@
-/*$(function () {
-  [
-    { id: 'navbar', url: '/victor-doblaje/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/victor-doblaje/components/footer.html', callback: initFooter },
-    { id: 'hero', url: '/victor-doblaje/sections/landing/hero.html' },
-    { id: 'services', url: '/victor-doblaje/sections/landing/services.html' },
-    { id: 'demos', url: '/victor-doblaje/sections/landing/demos.html', callback: initDemos },
-    { id: 'contact', url: '/victor-doblaje/sections/landing/contact.html', callback: initContact },
-  ].forEach(c => loadComponent(c.id, c.url, c.callback));
-});
-*/
-
-
 $(function () {
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar, },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'hero', url: '/assets/sections/landing/hero.html' },
-    { id: 'services', url: '/assets/sections/landing/services.html' },
-    { id: 'demos', url: '/assets/sections/landing/demos.html', callback: initDemos },
-    { id: 'contact', url: '/assets/sections/landing/contact.html', callback: initContact },
+    { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar, },
+    { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'hero', url: '/victormoreira/assets/sections/landing/hero.html' },
+    { id: 'services', url: '/victormoreira/assets/sections/landing/services.html' },
+    { id: 'demos', url: '/victormoreira/assets/sections/landing/demos.html', callback: initDemos },
+    { id: 'contact', url: '/victormoreira/assets/sections/landing/contact.html', callback: initContact },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 });
