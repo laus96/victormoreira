291ba794c541e67ce0a737d3303988ccdd224487
Laura Saro <78028957+laus96@users.noreply.github.com>
2025-11-07 21:30:10 +0100

    Update doblaje.js

--- docs/assets/scripts/sections/doblaje.js
+++ docs/assets/scripts/sections/doblaje.js
@@ -1,26 +1,14 @@
 $(function () {
 
   [
-    { id: 'navbar', url: '/assets/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/assets/components/footer.html', callback: initFooter },
-    { id: 'hero', url: '/assets/sections/doblaje/hero-sub.html' },
-    { id: 'demos', url: '/assets/sections/doblaje/demos-sub.html', callback: initPortfolio  },
-    { id: 'services', url: '/assets/sections/doblaje/services-sub.html' },
+    { id: 'navbar', url: '/victormoreira/assets/components/navbar.html', callback: initNavbar },
+    { id: 'footer', url: '/victormoreira/assets/components/footer.html', callback: initFooter },
+    { id: 'hero', url: '/victormoreira/assets/sections/doblaje/hero-sub.html' },
+    { id: 'demos', url: '/victormoreira/assets/sections/doblaje/demos-sub.html', callback: initPortfolio  },
+    { id: 'services', url: '/victormoreira/assets/sections/doblaje/services-sub.html' },
   ].forEach(c => loadComponent(c.id, c.url, c.callback));
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     setActiveNavbarButton("nav-doblaje");
 });
-
-
-/*
-$(function () {
-  [
-    { id: 'navbar', url: '/victor-doblaje/components/navbar.html', callback: initNavbar },
-    { id: 'footer', url: '/victor-doblaje/components/footer.html', callback: initFooter },
-    { id: 'legal', url: '/victor-doblaje/sections/aviso-legal/cookies.html', callback: initLegal },
-  ].forEach(c => loadComponent(c.id, c.url, c.callback));
-
-});
-*/
